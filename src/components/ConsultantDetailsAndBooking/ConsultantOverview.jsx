026818f121a6b1d68532bf6f700e05ebf626311e
Samuel M. O <samuel93ortil@gmail.com>
2023-08-21 20:42:36 +0100

    Consultant Details and Booking

--- src/components/ConsultantDetailsAndBooking/ConsultantOverview.jsx
+++ src/components/ConsultantDetailsAndBooking/ConsultantOverview.jsx
@@ -4,7 +4,10 @@ import { Link, useParams } from "react-router-dom";
 import { ChevronsRight, Home, MapPin } from "react-feather";
 import { ConsultantsInfo } from "../Consultants/ConsultantsData.js";
 import { useEffect, useState } from "react";
-import star from "../../assets/media/icons//Home/Consultants/icons/star.png";
+import star from "../../assets/media/icons/Home/Consultants/icons/star.png";
+import chat from "../../assets/media/icons/Home/Consultants/icons/circle_chat.png";
+import video from "../../assets/media/icons/Home/Consultants/icons/circle_video.png";
+import { InlineWidget } from "react-calendly";
 
 const ConsultantOverview = () => {
   const { consultantId } = useParams();
@@ -50,8 +53,9 @@ const ConsultantOverview = () => {
           </span>
         </div>
 
-        <div className="flex mt-12">
-          <div className="w-[220px]">
+        {/* Consultant Details */}
+        <div className="flex mt-12 gap-10">
+          <div className="w-[250px]">
             <img src={consultant?.pic} alt={consultant?.name} />
             <h5 className="text-center text-[18px] font-ManropeSemiBold py-3 text-BrandGray900">
               {consultant?.name}
@@ -72,7 +76,124 @@ const ConsultantOverview = () => {
               </span>
             </p>
           </div>
-          <div></div>
+          <div className="w-full">
+            <div>
+              <h3 className="font-ManropeBold text-BrandPrimary text-[20px]">
+                Specialization
+              </h3>
+              <div className="flex gap-5 pt-5 pb-10">
+                {consultant?.specialization.map((specialize, i) => {
+                  return (
+                    <span
+                      key={i}
+                      className="bg-BrandGray200 px-3 py-1 rounded-md font-ManropeSemiBold text-[14px]"
+                    >
+                      {specialize}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+            <hr className="border-[1px] bg-BrandGray300 my-5" />
+            <div>
+              <h3 className="font-ManropeBold text-BrandPrimary text-[20px]">
+                Services
+              </h3>
+              <div className="pt-5">
+                <div className="flex items-center gap-7">
+                  <img src={chat} alt="chat" className="w-[35px]" />
+                  <span className="font-ManropeBold text-BrandGray900">
+                    Unlimited
+                  </span>
+                  <span className="font-ManropeRegular bg-red-600 text-[14px] text-white px-2 rounded-md">
+                    FREE
+                  </span>
+                </div>
+              </div>
+              <div className="py-4">
+                <div className="flex items-center gap-7">
+                  <img src={video} alt="chat" className="w-[35px]" />
+                  <span className="font-ManropeBold text-BrandGray900">
+                    15 mins Session
+                  </span>
+                  <span className="font-ManropeRegular bg-[#22C55E] text-[14px] text-white px-2 rounded-md">
+                    {consultant?.fifteenMins}
+                  </span>
+
+                  <div className="w-[1px] h-[35px] border bg-BrandPrimary"></div>
+
+                  <span className="font-ManropeBold text-BrandGray900">
+                    30 mins Session
+                  </span>
+                  <span className="font-ManropeRegular bg-[#22C55E] text-[14px] text-white px-2 rounded-md">
+                    {consultant?.thirtyMins}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr className="border-[1px] bg-BrandGray300 my-5" />
+
+        {/* Booking Section */}
+        <div>
+          <h3 className="font-ManropeBold text-BrandPrimary text-[20px]">
+            Book Consultation
+          </h3>
+
+          <InlineWidget url="https://calendly.com/ictzoid/30min" />
+
+          <div>
+            <label
+              htmlFor="message"
+              className="block font-ManropeSemiBold text-[15px] text-BrandGray800 pt-5 pb-2"
+            >
+              Consultation Brief
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="5"
+              placeholder="Write a short note describing your consultation need..."
+              className="w-full rounded-md outline-none p-2 border"
+            ></textarea>
+          </div>
+
+          <button className="bg-BrandPrimary text-white px-3 py-2 font-ManropeRegular text-[14px] rounded-md shadow-md my-5">
+            Make an appointment
+          </button>
+        </div>
+
+        <hr className="border-[1px] bg-BrandGray300 my-5" />
+
+        {/* Reviews */}
+        <div>
+          <h3 className="font-ManropeBold text-BrandPrimary text-[20px]">
+            Reviews
+          </h3>
+
+          <div>
+            {consultant?.reviews.map((review, i) => {
+              return (
+                <div key={i} className="py-5">
+                  <p className="flex items-center font-ManropeSemiBold text-BrandGray800 gap-4">
+                    <span className="bg-BrandGray200 rounded-md px-2 py-1 text-[13px]">
+                      {review.tag}
+                    </span>
+                    <span className="text-[13px]">{review.date}</span>
+                  </p>
+                  <p className="text-BrandGray700 text-[15px] font-ManropeRegular py-2">
+                    {review.text}
+                  </p>
+                  <p className="font-ManropeSemiBold text-BrandGray900">
+                    {review.studentName}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
       <Footer />
--- src/components/Consultants/ConsultantsData.js
+++ src/components/Consultants/ConsultantsData.js
@@ -14,18 +14,25 @@ export const ConsultantsInfo = [
       {
         studentName: "Jane Cooper",
         text: "Tristique odio at dignissim viverra aliquet eleifend erat. Tellus, at arcu, egestas praesent. Varius aliquet pharetra adipiscing tincidunt orci nec neque. Massa nec varius arcu odio turpis felis a, eget leo.",
+        date: "Aug 19, 2023",
+        tag: "Admission",
       },
       {
         studentName: "John Doe",
         text: "Nunc aliquet scelerisque pellentesque imperdiet tortor elit, dictum. ",
+        date: "Mar 9, 2023",
+        tag: "Visa",
       },
       {
         studentName: "Peter Doe",
         text: "Nunc aliquet scelerisque pellentesque imperdiet tortor elit, dictum. ",
+        date: "Jun 9, 2023",
+        tag: "Scholarship",
       },
     ],
     stars: 3,
-    charges: [{ fifteenMins: "$10" }, { thirtyMins: "$10" }],
+    fifteenMins: "$10",
+    thirtyMins: "$20",
   },
   {
     id: 2,
@@ -37,14 +44,19 @@ export const ConsultantsInfo = [
       {
         studentName: "Jane Cooper",
         text: "Tristique odio at dignissim viverra aliquet eleifend erat. Tellus, at arcu, egestas praesent. Varius aliquet pharetra adipiscing tincidunt orci nec neque. Massa nec varius arcu odio turpis felis a, eget leo.",
+        date: "Jun 9, 2023",
+        tag: "Admission",
       },
       {
         studentName: "Alex Doe",
         text: "Nunc aliquet scelerisque pellentesque imperdiet tortor elit, dictum. ",
+        date: "Jun 20, 2023",
+        tag: "Admission",
       },
     ],
     stars: 2,
-    charges: [{ fifteenMins: "$20" }, { thirtyMins: "$30" }],
+    fifteenMins: "$15",
+    thirtyMins: "$25",
   },
 
   {
@@ -57,14 +69,19 @@ export const ConsultantsInfo = [
       {
         studentName: "Jane Cooper",
         text: "Tristique odio at dignissim viverra aliquet eleifend erat. Tellus, at arcu, egestas praesent. Varius aliquet pharetra adipiscing tincidunt orci nec neque. Massa nec varius arcu odio turpis felis a, eget leo.",
+        date: "Jun 9, 2021",
+        tag: "Admission",
       },
       {
         studentName: "Alex Doe",
         text: "Nunc aliquet scelerisque pellentesque imperdiet tortor elit, dictum. ",
+        date: "Jun 9, 2023",
+        tag: "Scholarship",
       },
     ],
     stars: 2,
-    charges: [{ fifteenMins: "$20" }, { thirtyMins: "$30" }],
+    fifteenMins: "$15",
+    thirtyMins: "$25",
   },
   {
     id: 4,
@@ -76,21 +93,30 @@ export const ConsultantsInfo = [
       {
         studentName: "Jane Cooper",
         text: "Tristique odio at dignissim viverra aliquet eleifend erat. Tellus, at arcu, egestas praesent. Varius aliquet pharetra adipiscing tincidunt orci nec neque. Massa nec varius arcu odio turpis felis a, eget leo.",
+        date: "Jun 9, 2023",
+        tag: "Admission",
       },
       {
         studentName: "Alex Doe",
         text: "Nunc aliquet scelerisque pellentesque imperdiet tortor elit, dictum. ",
+        date: "Jun 19, 2023",
+        tag: "Scholarship",
       },
       {
         studentName: "Paul Cooper",
         text: "Tristique odio at dignissim viverra aliquet eleifend erat. Tellus, at arcu, egestas praesent. Varius aliquet pharetra adipiscing tincidunt orci nec neque. Massa nec varius arcu odio turpis felis a, eget leo.",
+        date: "Jun 9, 2023",
+        tag: "Admission",
       },
       {
         studentName: "Zed Doe",
         text: "Nunc aliquet scelerisque pellentesque imperdiet tortor elit, dictum. ",
+        date: "Jun 9, 2023",
+        tag: "Loan",
       },
     ],
     stars: 4,
-    charges: [{ fifteenMins: "$25" }, { thirtyMins: "$30" }],
+    fifteenMins: "$25",
+    thirtyMins: "$30",
   },
 ];
